a33e86b979dd116f0594469964476add143607e5
Vagabond <marcusjjreyes@gmail.com>
2023-09-06 15:53:52 -0700

    added add role, update employee, update role. Working proto

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ async function queryEmployees() {
     })
 }
 
+async function queryDepartments() {
+    return new Promise((resolve, reject) => {
+        db.query("SELECT * FROM department", function (err, results) {
+            if (err) return reject(err);
+            resolve(results);
+        });
+    })
+}
 
 function init() {
     inquirer.prompt({
@@ -41,7 +49,7 @@ function init() {
             "add a department",
             "add a role",
             "add an employee",
-            "update an employee role",
+            "update an employeeRole",
             "EXIT",
         ]
     }).then((answers) => {
@@ -64,8 +72,8 @@ function init() {
             case "add an employee":
                 addEmployee();
                 break;
-            case "update an employee":
-                updateEmployee();
+            case "update an employeeRole":
+                updateEmployeeRole();
                 break;
             case "EXIT":
                 db.end();
@@ -82,8 +90,8 @@ function viewAllDepartments() {
         }
         console.log("All Departments")
         console.table(res);
+        init();
     });
-    init();
 }
 
 function viewAllRoles() {
@@ -93,8 +101,9 @@ function viewAllRoles() {
         }
         console.log("Viewing all roles");
         console.table(res);
+        init();
     });
-    init();
+
 
 }
 
@@ -105,12 +114,12 @@ function viewAllEmployees() {
         }
         console.log("Viewing all employees");
         console.table(res);
+        init();
     });
-    init();
+
 }
 
 function addDepartment() {
-    console.log("hi");
     inquirer.prompt({
 
         type: "input",
@@ -127,20 +136,51 @@ function addDepartment() {
             }
         )
 
-        init();
+        // init();
     });
 
 }
 
-function addRole() {
-    console.log("hi");
+async function addRole() {
+    const departments = await queryDepartments();
+    inquirer.prompt([
+     {
+        type: "input",
+        name: "roleName",
+        message: "What is the name of the new role?"
+     },
+     {
+        type: "input",
+        name: "salary",
+        message: "What is the salary of this role?"
+    },
+    {
+        type: "list",
+        name: "department_id",
+        message: "What is the department this role belongs to?",
+        choices: departments.map((department) => ({ name: department.name, value: department.id}))
+    } 
+    ]).then((answers) => {
+        db.query("INSERT INTO role SET ?",
+            { 
+                id: 0,
+                title: answers.roleName,
+                salary: answers.salary,
+                department_id: answers.department_id
+            },
+            function (err) {
+                if (err) throw err;
+                init();
+            }
+        )
+
+        // init();
+    });
 }
 
 async function addEmployee() {
     const Roles = await queryRoles();
     const employees = await queryEmployees();
-    console.log(employees)
-    console.log(Roles)
     inquirer.prompt([
         {
             name: "firstName",
@@ -178,14 +218,47 @@ async function addEmployee() {
                 if (err) throw err;
                 init();
             }
-            
+
         )
     });
 
 }
 
-function updateEmployee() {
+async function updateEmployeeRole() {
+    const Roles = await queryRoles();
+    const employees = await queryEmployees();
+    inquirer.prompt([
+        {
+            name: "employeeToUpdate",
+            message: "Which employee would you like to update?",
+            type: "list",
+            choices: employees.map((employee) => ({ name: employee.first_name + " " + employee.last_name, value: employee.id }))
+        },
+        {
+            name: "roleId",
+            type: "list",
+            message: "What is the updated role?",
+            choices: Roles.map((role) => ({ name: role.title, value: role.id }))
+        },
+    ]).then((answers) => {
+        console.log(answers);
+        db.query("UPDATE employee SET ? WHERE ?",
+            [
+                {
+                    role_id: answers.roleId,
+                },
+                {
+                    id: answers.employeeToUpdate 
+                },
 
+            ],
+            function (err) {
+                if (err) throw err;
+                init();
+            }
+
+        )
+    });
 }
 
 init()

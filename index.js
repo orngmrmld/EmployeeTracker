6d0f4b4cfc647cccabaf967c3c572592fb84f046
Vagabond <marcusjjreyes@gmail.com>
2023-09-06 13:04:35 -0700

    populated more switche cases

--- index.js
+++ index.js
@@ -19,11 +19,40 @@ function init(){
         name: "firstQuestion",
         type: "list",
         message: "What do you want?",
-        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+        choices: [
+            "view all departments", 
+            "view all roles", 
+            "view all employees", 
+            "add a department", 
+            "add a role", 
+            "add an employee", 
+            "update an employee role",
+            "EXIT",
+    ]
     }).then((answers) => {
         switch(answers.firstQuestion){
             case "view all departments":
                 viewAllDepartments();
+                break;
+            case "view all roles":
+                viewAllRoles();
+                break;
+            case "view all employees":
+                viewAllEmployees();
+                break;
+            case "add department":
+                addDepartment();
+                break;
+            case "add role":
+                addRole();
+                break;
+            case "add employee":
+                addEmployee();
+                break;
+            case "update employee":
+                updateEmployee();
+                break;
+            case "EXIT":
                 break;
         }
     })
@@ -34,5 +63,28 @@ function viewAllDepartments() {
     console.log("Hello");
 }
 
+function viewAllRoles(){
+    console.log("Roles");
+}
+
+function viewAllEmployees(){
+    console.log("Employees")
+}
+
+function addDepartment(){
+
+}
+
+function addRole(){
+
+}
+
+function addEmployee(){
+
+}
+
+function updateEmployee(){
+
+}
 
 init()
